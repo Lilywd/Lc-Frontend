508d16ea28f284f66e25222cf47a590700786db6
principal kelvo <kelvinnjoroowino@gmail.com>
2023-06-10 13:00:25 +0300

    images

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,36 +1,49 @@
-export const leftNavLinks = [
+export const candleProducts = [
   {
-    id: "home",
-    title: "Home",
+    id: "candle",
+    img: "https://i.pinimg.com/originals/4e/c1/f9/4ec1f9082ff0295826d01344220b3752.jpg",
   },
   {
-    id: "features",
-    title: "Features",
+    id: "candle1",
+    img: "https://i.pinimg.com/originals/2f/cb/b3/2fcbb35c683ccba725f74f9c7db79970.jpg",
   },
   {
-    id: "classes",
-    title: "Classes",
+    id: "candle2",
+    img: "https://i.pinimg.com/originals/8a/3c/aa/8a3caa5ee85333288c17edf06c9d976e.jpg",
   },
   {
-    id: "clients",
-    title: "Clients",
+    id: "candle3",
+    img: "https://i.pinimg.com/originals/08/0a/37/080a37d2e7a748fb58887293051c5ad2.jpg",
+  },
+  {
+    id: "candle4",
+    img: "https://i.pinimg.com/originals/3c/14/79/3c1479d15766a8ad3fd5fbf7fe383b00.jpg",
   },
 ];
-export const rightNavLinks = [
+
+export const covers = [
   {
-    id: "home",
-    title: "Home",
+    id: "cover",
+    img: "https://unsplash.com/photos/bdVmIkx_gIshttps://unsplash.com/photos/bdVmIkx_gIs",
   },
   {
-    id: "features",
-    title: "Features",
+    id: "cover1",
+    img: "hhttps://unsplash.com/photos/cCFWIye3sR8",
   },
   {
-    id: "classes",
-    title: "Classes",
+    id: "cover2",
+    img: "https://unsplash.com/photos/iSyyY1GfYSw",
   },
   {
-    id: "clients",
-    title: "Clients",
+    id: "cover3",
+    img: "https://unsplash.com/photos/8wMcY9OBgqs",
+  },
+  {
+    id: "cover4",
+    img: "https://unsplash.com/photos/BZW67ux31KU",
+  },
+  {
+    id: "cover5",
+    img: "https://unsplash.com/photos/iKHXbvNHXPQ",
   },
 ];
