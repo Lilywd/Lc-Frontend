66450739ea25cfd0f1993a20d94b2b2caa0bc6ca
Evans <evansroysir@gmail.com>
2023-06-23 19:24:27 +0300

    Add Route to product page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Cart from "./components/cart/Cart";
 import CheckoutForm from "./components/cart/Checkoutform";
+import Product from "./pages/product/Product";
 // import Hero from "./components/home/Hero";
 
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route exact path="/cart" element={<Cart />} />
         <Route exact path="/checkout" element={<CheckoutForm />} />
+        <Route exact path="/product" element={<Product />} />
       </Routes>
     </>
   );
